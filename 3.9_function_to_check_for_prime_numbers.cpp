1ed6bfc8967099d346c4be215f5a65cf2668cd8e
GustiG <118075768+GustiG@users.noreply.github.com>
2023-08-10 21:04:49 +0300

    Add files via upload
    A prime number is a number that can only be divided by itself and 1 without remainders.

---
+++ 3.9_function_to_check_for_prime_numbers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// function to check prime number
+int check_prime(int n) {
+
+    // 0 and 1 are not prime numbers
+    if (n == 0 || n == 1) {
+        return 0;
+    }
+
+    for (int i = 2; i <= n / 2; ++i) {
+        if (n % i == 0)
+            return 0;
+    }
+
+    return 1;
+}
+
+int main() {
+    bool result = check_prime(67);
+
+    if (result == 1)
+        cout << "Prime Number";
+    else
+        cout << "Non-prime Number";
+
+    return 0;
+}
